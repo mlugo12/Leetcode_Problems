ead3568c5c2d50c8369e70e52fe01521bede4694
Miguel <lugomiguel65@yahoo.com>
2023-12-13 17:03:12 -0500

    Testing

--- .github/workflows/blank.java
+++ .github/workflows/blank.java
@@ -1,2 +1,6 @@
-testing
-  
+class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("this is a test to see if it works ");
+    }
+}
