ca7b842a41d14e53eb937076daf8acc70c9792bb
Miguel <lugomiguel65@yahoo.com>
2023-12-14 14:26:59 -0500

    Final Product

--- .github/workflows/IntegerToRoman.java
+++ .github/workflows/IntegerToRoman.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("this is a test to see if it works ");
+        RomanNumber(105);
     }
+
+    public static void RomanNumber (int num){
+        String output = "";
+        int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] roman = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = values.length-1; i >= 0 && num > 0; i--){
+          while (num >= values[i]){
+            num -= values[i];
+            sb.append(roman[i]);
+          }  
+        }
+        System.out.println(sb.toString());
+    }
+
+     
+
 }
